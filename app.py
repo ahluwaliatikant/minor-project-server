0c68db4744f12fe12e9035bdbd990d997d6f2afa
ahluwaliatikant <atikant.ahluwalia@gmail.com>
2022-12-18 17:33:12 +0530

    initial server files upload

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, Markup, jsonify
+import numpy as np
+import pandas as pd
+import requests
+import pickle
+import io
+
+
+# Loading crop recommendation model
+
+crop_recommendation_model_path = 'RandomForest.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+app = Flask(__name__)
+
+
+# Weather Fetch API
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = "9d7cde1f6d07ec55650544be1631307e"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+
+# Crop Prediction API
+
+@ app.route('/crop-predict', methods=['GET'])
+def crop_prediction():
+    title = 'Harvestify - Crop Recommendation'
+
+    if request.method == 'GET':
+        N = int(request.args.get('N'))
+        P = int(request.args.get('P'))
+        K = int(request.args.get('K'))
+        ph = float(request.args.get('ph'))
+        rainfall = float(request.args.get('rainfall'))
+
+        # state = request.form.get("stt")
+        city = request.args.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            print(data)
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+
+            crop_prediction_data_to_be_returned = {'prediction': final_prediction}
+            return jsonify(crop_prediction_data_to_be_returned)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+
+# ===============================================================================================
+if __name__ == '__main__':
+    app.run(debug=False)
